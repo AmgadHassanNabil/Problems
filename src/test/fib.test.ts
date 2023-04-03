08835342851ecc08741b0bc48f19228b4928176a
AmgadHassanNabil <ahntawfik@hotmail.com>
2023-04-03 20:03:35 +0200

    moved index.ts

--- src/test/fib.test.ts
+++ src/test/fib.test.ts
@@ -9,5 +9,6 @@ describe('Fib Module', () =>
         expect(fib.fib(2)).toBe(1);
         expect(fib.fib(3)).toBe(2);
         expect(fib.fib(4)).toBe(3);
+        expect(fib.fib(6)).toBe(8);
     });
 });
